2183eb63898430dc299fa274bd9b69e35a31df10
morteza yaghoobi <morteza.yaghoobi1375@gmail.com>
2022-04-08 18:24:33 +0430

    implement use cases

---
+++ app/src/main/java/ir/moris/cripotoid/domain/use_cases/get_cripto/GetCryptoUseCase.kt
@@ -0,0 +1,33 @@
+package ir.moris.cripotoid.domain.use_cases.get_cripto
+
+import ir.moris.cripotoid.common.Resource
+import ir.moris.cripotoid.data.remote.dto.toCrypto
+import ir.moris.cripotoid.data.remote.dto.toCryptoDetail
+import ir.moris.cripotoid.domain.model.Crypto
+import ir.moris.cripotoid.domain.model.CryptoDetail
+import ir.moris.cripotoid.domain.repositorie.CryptoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCryptoUseCase @Inject constructor(
+    private val repository: CryptoRepository
+){
+
+    operator fun invoke(coinId: String): Flow<Resource<CryptoDetail>> {
+        return flow {
+            try {
+                emit(Resource.Loading())
+                val crypto = repository.getCryptoById(coinId).toCryptoDetail()
+                emit(Resource.Success(crypto))
+            }catch (e : HttpException){
+                emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
+            }catch (e : IOException){
+                emit(Resource.Error("couldn't reach server. check your internet connection"))
+            }
+        }
+    }
+
+}
